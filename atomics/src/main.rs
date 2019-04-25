01cb4b5c521b86a74620f3e4aa3651b36b7f72dc
Jack O'Connor <oconnor663@gmail.com>
2019-04-25 01:55:51 -0400

    ordering example

--- atomics/src/main.rs
+++ atomics/src/main.rs
@@ -1,27 +1,20 @@
 use num_format::{Locale, ToFormattedString};
 use std::sync::atomic::{AtomicU64, Ordering};
-use std::sync::Arc;
 use std::time::Duration;
 
+static MY_INT: AtomicU64 = AtomicU64::new(0);
+
 fn main() {
-    let my_int = Arc::new(AtomicU64::new(0));
+    std::thread::spawn(move || loop {
+        MY_INT.fetch_add(1, Ordering::SeqCst);
+    });
 
-    {
-        let my_int = my_int.clone();
-        std::thread::spawn(move || loop {
-            my_int.fetch_add(1, Ordering::SeqCst);
-        });
-    }
-
-    {
-        let my_int = my_int.clone();
-        std::thread::spawn(move || loop {
-            my_int.fetch_add(1, Ordering::SeqCst);
-        });
-    }
+    std::thread::spawn(move || loop {
+        MY_INT.fetch_add(1, Ordering::SeqCst);
+    });
 
     loop {
-        let val = my_int.load(Ordering::SeqCst);
+        let val = MY_INT.load(Ordering::SeqCst);
         println!("{}", val.to_formatted_string(&Locale::en));
         std::thread::sleep(Duration::from_secs(1));
     }
