8a15ec18b00a823a3a3a5aa3bed9671b83b35211
Jack O'Connor <oconnor663@gmail.com>
2019-05-08 13:14:47 -0400

    simplify the ordering example

--- ordering/src/main.rs
+++ ordering/src/main.rs
@@ -1,50 +1,31 @@
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::mpsc::sync_channel;
-use std::sync::{Arc, Barrier};
 
-// const MY_ORDERING: Ordering = Ordering::SeqCst;
 const MY_ORDERING: Ordering = Ordering::Relaxed;
 
 static A: AtomicBool = AtomicBool::new(false);
 static B: AtomicBool = AtomicBool::new(false);
 
 fn main() {
-    let barrier = Arc::new(Barrier::new(3));
-
-    // Worker thread 1 sets A and then reads B.
-    let barrier_clone = barrier.clone();
-    let (sender1, receiver1) = sync_channel(0);
-    std::thread::spawn(move || loop {
-        barrier_clone.wait();
-        A.store(true, MY_ORDERING);
-        let read = B.load(MY_ORDERING);
-        sender1.send(read).unwrap();
-    });
-
-    // Worker thread 2 sets B and then reads A.
-    let barrier_clone = barrier.clone();
-    let (sender2, receiver2) = sync_channel(0);
-    std::thread::spawn(move || loop {
-        barrier_clone.wait();
-        B.store(true, MY_ORDERING);
-        let read = A.load(MY_ORDERING);
-        sender2.send(read).unwrap();
-    });
-
-    // The main thread intializes A and B, and then signals the worker threads
-    // by triggering the Barrier. It receives the read from each thread and
-    // checks them for sequential consistency.
     let mut iterations = 0u64;
     loop {
         iterations += 1;
         A.store(false, MY_ORDERING);
         B.store(false, MY_ORDERING);
-        barrier.wait();
-        let read1 = receiver1.recv().unwrap();
-        let read2 = receiver2.recv().unwrap();
-        if !read1 && !read2 {
-            println!("Impossible result after {} iterations!", iterations);
-            return;
+
+        let a_child = std::thread::spawn(|| {
+            A.store(true, MY_ORDERING);
+            B.load(MY_ORDERING)
+        });
+
+        let b_child = std::thread::spawn(|| {
+            B.store(true, MY_ORDERING);
+            A.load(MY_ORDERING)
+        });
+
+        let b_read = a_child.join().unwrap();
+        let a_read = b_child.join().unwrap();
+        if a_read == false && b_read == false {
+            panic!("inconsistent result after {} iterations", iterations);
         }
     }
 }
