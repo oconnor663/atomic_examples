bbb524dbd0d0763e7c7af685bb04b91861c03cb6
Jack O'Connor <oconnor663@gmail.com>
2019-05-09 02:26:01 -0400

    the complicated ordering example is more reliable

--- ordering/src/main.rs
+++ ordering/src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{mpsc, Arc, Barrier};
 
 const MY_ORDERING: Ordering = Ordering::Relaxed;
 
@@ -6,25 +7,35 @@ static A: AtomicBool = AtomicBool::new(false);
 static B: AtomicBool = AtomicBool::new(false);
 
 fn main() {
+    let barrier = Arc::new(Barrier::new(3));
+
+    let barrier1 = barrier.clone();
+    let (sender1, receiver1) = mpsc::sync_channel(0);
+    std::thread::spawn(move || loop {
+        barrier1.wait();
+        A.store(true, MY_ORDERING);
+        let b = B.load(MY_ORDERING);
+        sender1.send(b).unwrap();
+    });
+
+    let barrier2 = barrier.clone();
+    let (sender2, receiver2) = mpsc::sync_channel(0);
+    std::thread::spawn(move || loop {
+        barrier2.wait();
+        B.store(true, MY_ORDERING);
+        let a = A.load(MY_ORDERING);
+        sender2.send(a).unwrap();
+    });
+
     let mut iterations = 0u64;
     loop {
         iterations += 1;
         A.store(false, MY_ORDERING);
         B.store(false, MY_ORDERING);
-
-        let a_child = std::thread::spawn(|| {
-            A.store(true, MY_ORDERING);
-            B.load(MY_ORDERING)
-        });
-
-        let b_child = std::thread::spawn(|| {
-            B.store(true, MY_ORDERING);
-            A.load(MY_ORDERING)
-        });
-
-        let b_read = a_child.join().unwrap();
-        let a_read = b_child.join().unwrap();
-        if a_read == false && b_read == false {
+        barrier.wait();
+        let a = receiver2.recv().unwrap();
+        let b = receiver1.recv().unwrap();
+        if a == false && b == false {
             panic!("inconsistent result after {} iterations", iterations);
         }
     }
